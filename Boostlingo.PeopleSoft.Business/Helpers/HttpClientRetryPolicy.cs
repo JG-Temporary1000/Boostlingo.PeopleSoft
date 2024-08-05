d5e4f9e95f053cd3cad44b7feb410851808c0a18
Your New Username Josh <jg@codemoto.io>
2024-08-04 21:55:58 -0600

    Added HttpClientRetryPolicy to harden API service (typed implemenation of HTTPClient).

---
+++ Boostlingo.PeopleSoft.Business/Helpers/HttpClientRetryPolicy.cs
@@ -0,0 +1,16 @@
+﻿using Polly.Extensions.Http;
+using Polly;
+
+namespace Boostlingo.PeopleSoft.Business.Helpers
+{
+    public class HttpClientRetryPolicy
+    {
+        public static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+        {
+            return HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+                .WaitAndRetryAsync(6, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+        }
+    }
+}
